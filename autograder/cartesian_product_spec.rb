9ebf35e99dad39de5e78c4498e48753ac0751741
unccsestaff <unccsestaff@gmail.com>
2017-02-03 19:55:09 -0500

    Added more test cases.

--- autograder/cartesian_product_spec.rb
+++ autograder/cartesian_product_spec.rb
@@ -1,6 +1,15 @@
 describe "CartesianProduct" do
-  it "should correctly compute the product of two arrays [100 points]", points: 100 do
+  it "should correctly show the product of two arrays [25 points]", points: 25 do
     CartesianProduct.new([1,2,3],['a','b']).to_a.should == [1,2,3].product(['a','b'])
+  end
+  it "should correctly show the product of two arrays [25 points]", points: 25 do
+    CartesianProduct.new([10, 9, 3],['a','b', 't', 'q']).to_a.should == [10, 9, 3].product(['a','b','t', 'q'])
+  end
+  it "should work with an empty list [25 points]", points: 25 do
+    CartesianProduct.new.to_a.should == [[]]
+  end
+  it "should correctly show the product of a single array [25 points]", points: 25 do
+    CartesianProduct.new([1,2,3]).to_a.should == [[1],[2],[3]]
   end
 end
   
