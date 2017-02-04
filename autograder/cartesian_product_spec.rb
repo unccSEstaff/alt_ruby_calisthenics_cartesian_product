3b3c2c335370223179e87ae0238231b101865bd3
unccsestaff <unccsestaff@gmail.com>
2017-02-03 20:06:30 -0500

    Fixed incorrect should statement for third test.

--- autograder/cartesian_product_spec.rb
+++ autograder/cartesian_product_spec.rb
@@ -6,7 +6,7 @@ describe "CartesianProduct" do
     CartesianProduct.new([10, 9, 3],['a','b', 't', 'q']).to_a.should == [10, 9, 3].product(['a','b','t', 'q'])
   end
   it "should correctly show the product of two arrays [25 points]", points: 25 do
-    CartesianProduct.new([10,3,5,8,2,1],['z', 'g', 3, 'a','b', 't', 'q']).to_a.should == [10, 9, 3].product(['a','b','t', 'q'])
+    CartesianProduct.new([10,3,5,8,2,1],['z', 'g', 3, 'a','b', 't', 'q']).to_a.should == [10,3,5,8,2,1].product(['z', 'g', 3, 'a','b', 't', 'q'])
   end
   it "should correctly show the product of two arrays [25 points]", points: 25 do
     CartesianProduct.new([1], ['a']).to_a.should == [1].product(['a'])
